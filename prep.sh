1f20bcb1c6c823944c134132e1c5fbe245f07752
Daniel Rinehart <danielr@neophi.com>
2013-03-08 09:16:28 -0500

    Tweak prep script to copy things into correct locations.

--- prep.sh
+++ prep.sh
@@ -1,10 +1,11 @@
 rm -f -r ../jellyDeploy/src/
 mkdir ../jellyDeploy/src/
+cp -R src/client ../jellyDeploy/src/
 cp -R src/server ../jellyDeploy/src/
 cp -R src/views ../jellyDeploy/src/
 
 rm -f -r ../jellyDeploy/static/
 cp -R static ../jellyDeploy/
-cp -R build ../jellyDeploy/
+cp -R build/ ../jellyDeploy/static/
 
 cp -f Procfile package.json ../jellyDeploy/
